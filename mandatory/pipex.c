5125c0944d44eac853505af7b4bb253c2c7272a5
jean <jduval@3E6.42angouleme.fr>
2023-02-22 14:27:45 +0100

    adjust of argc in parsing to main

--- mandatory/pipex.c
+++ mandatory/pipex.c
@@ -6,7 +6,7 @@
 /*   By: jduval <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 08:41:01 by jduval            #+#    #+#             */
-/*   Updated: 2023/02/22 14:13:03 by jduval           ###   ########.fr       */
+/*   Updated: 2023/02/22 14:26:33 by jduval           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,11 @@ int	main(int argc, char **argv, char **envp)
 {
 	t_cmd	*cmd;
 
+	if (argc != 5)
+	{
+		ft_putstr_fd("Bad pathern !\nNeed 5 arguments\n", 2);
+		return (0);
+	}
 	cmd = ft_parsing(argc, argv, envp);
 	ft_loop_on_cmd(cmd, argv, envp);
 	return (0);
--- mandatory/px_parsing.c
+++ mandatory/px_parsing.c
@@ -6,7 +6,7 @@
 /*   By: jduval <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 13:54:48 by jduval            #+#    #+#             */
-/*   Updated: 2023/02/20 15:47:21 by jduval           ###   ########.fr       */
+/*   Updated: 2023/02/22 14:24:46 by jduval           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,6 @@ t_cmd	*ft_parsing(int argc, char **argv, char **envp)
 	t_cmd	*cmd;
 	char	**path;
 
-	if (argc != 5)
-	{
-		ft_printf("ERROR\nPathern: ./pipex 'file1' 'cmd1' 'cmd2' 'file2'\n");
-		exit (0);
-	}
 	path = ft_envp_to_path(envp);
 	cmd = ft_create_chain(argc, argv, path);
 	if (cmd == NULL)
